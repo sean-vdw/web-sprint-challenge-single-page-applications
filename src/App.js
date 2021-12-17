37b685566fa1d1367be1946513e06cc71d0f1fb4
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-17 08:54:44 -0800

    updated initial order in app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import { Route, Link, useHistory } from 'react-router-dom';
 import axios from "axios";
 
 
+
+const initialOrder = {
+  name: '',
+  size: '',
+  sauce: '',
+  toppings: '',
+  special: '',
+}
+
 const App = () => {
+  const [orders, setOrders] = useState([initialOrder]);
+
   return (
     <>
 
@@ -17,9 +28,18 @@ const App = () => {
         </nav>
         <div className='hero'>
           <h1>Pizza Delivery for Coders &#127829;</h1>
-          <button className='orderBtn'>I'm Hungry!</button>
+          <Link to='/pizza'>
+            <button className='order-pizza'>Order Now</button>
+          </Link>
         </div>
       </header>
+      <div className='cta'>
+          <h2>Pizza delivery to you in <span>60 seconds.</span></h2>
+          <p>Because coding is better when you're not hungry.</p>
+          <Link to='/pizza'>
+            <button className='order-pizza'>Order Now</button>
+          </Link>
+      </div>
       <Route path='/pizza'>
 
       </Route>
