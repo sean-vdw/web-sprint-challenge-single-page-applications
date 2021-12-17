5e44104282031df3f2a160aa2c98f9e12c5f842f
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-17 08:33:09 -0800

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-router-dom';
+import { Route, Link, useHistory } from 'react-router-dom';
+import axios from "axios";
+
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+
+    <div className='lambdaEats'>
+      <header>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/pizza'>Order Pizza</Link>
+          <Link to='/order'>My Order</Link>
+        </nav>
+        <div className='hero'>
+          <h1>Pizza Delivery for Coders &#127829;</h1>
+          <button className='orderBtn'>I'm Hungry!</button>
+        </div>
+      </header>
+      <Route path='/pizza'>
+
+      </Route>
+      <Route path='/order'>
+
+      </Route>
+    </div>
+
     </>
   );
 };
+
 export default App;
